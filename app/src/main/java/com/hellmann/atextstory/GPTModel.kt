04f1ae79c63de8ba96a8afb88303229a6084b7a3
Cicero Hellmann <cicerohellmann@gmail.com>
2023-09-23 19:14:32 +0200

    Create GPTModel.kt
    Containing all available GPT models for the chat endpoint

---
+++ app/src/main/java/com/hellmann/atextstory/GPTModel.kt
@@ -0,0 +1,14 @@
+package com.hellmann.atextstory
+
+object GPTModel {
+    val gpt_3_5_turbo = "gpt-3.5-turbo"
+    val gpt_3_5_turbo_0301 = "gpt-3.5-turbo-0301"
+    val gpt_3_5_turbo_0613 = "gpt-3.5-turbo-0613"
+    val gpt_3_5_turbo_16k = "gpt-3.5-turbo-16k"
+    val gpt_3_5_turbo_16k_0613 = "gpt-3.5-turbo-16k-0613"
+    val gpt_3_5_turbo_instruct = "gpt-3.5-turbo-instruct"
+    val gpt_3_5_turbo_instruct_0914 = "gpt-3.5-turbo-instruct-0914"
+    val gpt_4 = "gpt-4"
+    val gpt_4_0314 = "gpt-4-0314"
+    val gpt_4_0613 = "gpt-4-0613"
+}
