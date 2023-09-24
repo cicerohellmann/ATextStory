4e966d3aa5c5ed3e711b29dcdb8b78efe3dae48c
Cicero Hellmann <cicerohellmann@gmail.com>
2023-09-24 15:59:59 +0200

    Update SystemDefinition.kt
    To prevent the JSON of being broken when GPT doesn't like the input

--- app/src/main/java/com/hellmann/atextstory/client/SystemDefinition.kt
+++ app/src/main/java/com/hellmann/atextstory/client/SystemDefinition.kt
@@ -5,6 +5,7 @@ fun systemDefinition(theme: String): String {
             "The story will be about $theme, " +
             "Your answers should be like " +
             "{\"scenario\":\"some_scenario\",\"options\":[\"some option here\",\"some other option here\",\"another option here and on and on\"]}\n " +
+            "Please adhere strictly to the specified JSON format, even in the case of sensitive or unexpected inputs. If the player attempts self-harm or any other inappropriate actions, end the game safely and provide options to restart. or giving options to call for help" +
             "where we could have an N amount of options, " +
             "the JSON must be unformatted, please"
 }
