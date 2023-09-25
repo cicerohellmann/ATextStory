4d4c49f3d4464439421213b696ac6ed46121c029
Cicero Hellmann <cicerohellmann@gmail.com>
2023-09-25 13:09:44 +0200

    Update SystemDefinition.kt
    With plain text to increase readability
    and moved the standard role and initial user message here to have a more concise view of the adventure setup

--- app/src/main/java/com/hellmann/atextstory/client/SystemDefinition.kt
+++ app/src/main/java/com/hellmann/atextstory/client/SystemDefinition.kt
@@ -1,11 +1,31 @@
 package com.hellmann.atextstory.client
 
+import com.hellmann.atextstory.data.Message
+
 fun systemDefinition(theme: String): String {
-    return "You will act as game master, " +
-            "The story will be about $theme, " +
-            "Your answers should be like " +
-            "{\"scenario\":\"some_scenario\",\"options\":[\"some option here\",\"some other option here\",\"another option here and on and on\"]}\n " +
-            "Please adhere strictly to the specified JSON format, even in the case of sensitive or unexpected inputs. If the player attempts self-harm or any other inappropriate actions, end the game safely and provide options to restart. or giving options to call for help" +
-            "where we could have an N amount of options, " +
-            "the JSON must be unformatted, please"
+    return """
+        You will act as game master,
+        
+        The story will be about $theme,
+        Your answers should be like
+        {"scenario":"some_scenario","options":["some option here, someTag","some other option here","another option here and on and on"]}
+        where we could have an N amount of options,
+        
+        Please adhere strictly to the specified JSON format, even in the case of sensitive or unexpected inputs.
+        If the player attempts self-harm or any other inappropriate actions, end the game safely and 
+        provide options to restart. or giving options to call for help,
+   
+        the JSON must be unformatted, please
+        
+    """.trimIndent().filterNot { it == '\n' || it == '\r' }
 }
+
+fun roleMessage(pickedTheme: String) = Message(
+    role = "system",
+    content = systemDefinition(pickedTheme)
+)
+
+val initialUserMessage = Message(
+    role = "user",
+    content = "Generate the adventure."
+)
