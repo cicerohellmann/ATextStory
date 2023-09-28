004cecef10824d2137b9b2cc333e277ef3e5d486
Cicero Hellmann <cicerohellmann@gmail.com>
2023-09-28 11:48:18 +0100

    Adding a test key

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,6 +10,7 @@ val properties = Properties().apply {
     load(project.rootProject.file("local.properties").inputStream())
 }
 val apiKey = properties.getProperty("apiKey") ?: "defaultApiKey"
+//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
 
 android {
     namespace = "com.hellmann.atextstory"
